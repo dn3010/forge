1fb8ef6eb66899c3d8a7612579dca4a9ca8ed1b3
Scott <skott.twiname@gmail.com>
2016-03-17 13:25:47 +1300

    Merge commonjs branch, apply sha512 fixes

--- js/md.js
+++ js/md.js
@@ -5,73 +5,20 @@
  *
  * Copyright 2011-2014 Digital Bazaar, Inc.
  */
-(function() {
-/* ########## Begin module implementation ########## */
-function initModule(forge) {
-
-forge.md = forge.md || {};
-forge.md.algorithms = {
-  md5: forge.md5,
-  sha1: forge.sha1,
-  sha256: forge.sha256,
-  sha512: forge.sha512
+var md5 = require("./md5");
+var sha1 = require("./sha1");
+var sha256 = require("./sha256");
+var sha512 = require("./sha512");
+var md = {};
+md.algorithms = {
+  md5: md5,
+  sha1: sha1,
+  sha256: sha256,
+  sha512: sha512
 };
-forge.md.md5 = forge.md5;
-forge.md.sha1 = forge.sha1;
-forge.md.sha256 = forge.sha256;
-forge.md.sha512 = forge.sha512;
+md.md5 = md5;
+md.sha1 = sha1;
+md.sha256 = sha256;
+md.sha512 = sha512;
 
-} // end module implementation
-
-/* ########## Begin module wrapper ########## */
-var name = 'md';
-if(typeof define !== 'function') {
-  // NodeJS -> AMD
-  if(typeof module === 'object' && module.exports) {
-    var nodeJS = true;
-    define = function(ids, factory) {
-      factory(require, module);
-    };
-  } else {
-    // <script>
-    if(typeof forge === 'undefined') {
-      forge = {};
-    }
-    return initModule(forge);
-  }
-}
-// AMD
-var deps;
-var defineFunc = function(require, module) {
-  module.exports = function(forge) {
-    var mods = deps.map(function(dep) {
-      return require(dep);
-    }).concat(initModule);
-    // handle circular dependencies
-    forge = forge || {};
-    forge.defined = forge.defined || {};
-    if(forge.defined[name]) {
-      return forge[name];
-    }
-    forge.defined[name] = true;
-    for(var i = 0; i < mods.length; ++i) {
-      mods[i](forge);
-    }
-    return forge[name];
-  };
-};
-var tmpDefine = define;
-define = function(ids, factory) {
-  deps = (typeof ids === 'string') ? factory.slice(2) : ids.slice(2);
-  if(nodeJS) {
-    delete define;
-    return tmpDefine.apply(null, Array.prototype.slice.call(arguments, 0));
-  }
-  define = tmpDefine;
-  return define.apply(null, Array.prototype.slice.call(arguments, 0));
-};
-define(
-  ['require', 'module', './md5', './sha1', './sha256', './sha512'], function() {
-  defineFunc.apply(null, Array.prototype.slice.call(arguments, 0));
-});
-})();
+module.exports = md;
